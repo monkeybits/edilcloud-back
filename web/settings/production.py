676db562545460f9e5f34fa665a81b9d74dcbd53
tommasobellini <dev@tommasobellini.it>
2020-07-02 21:08:17 +0200

    # Added docker compose prod with production settings

--- web/settings/production.py
+++ web/settings/production.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
 
-# Put here your production specific settings
+#!python
+from web.settings.base import *
 
 ADMINS = [
     ('THUX Team', 'jumbo@thux.it'),
 ]
-EMAIL_SUBJECT_PREFIX = '[WHISTLE]'
+EMAIL_SUBJECT_PREFIX = '[EDILCLOUD]'
 REST_FRAMEWORK_DOCS = {
     'HIDE_DOCS': True
 }
+SECRET_KEY = '393bf48ry80euefh7ye09euh2f2920hef29r03infbef82938eup13'
+BASE_URL = 'localhost:4000'

0ebce9a3414f77013a4b023ec5a384e6249735ed
tommasobellini <dev@tommasobellini.it>
2020-08-07 00:35:50 +0200

    # added base url

--- web/settings/production.py
+++ web/settings/production.py
@@ -11,7 +11,7 @@ REST_FRAMEWORK_DOCS = {
     'HIDE_DOCS': True
 }
 SECRET_KEY = '393bf48ry80euefh7ye09euh2f2920hef29r03infbef82938eup13'
-BASE_URL = 'localhost:3000'
+BASE_URL = 'www.edilcloud.it'
 
 STRIPE_PUBLISHABLE_KEY = '<your live publishable key here>'
 STRIPE_SECRET_KEY = '<your live secret key here>'

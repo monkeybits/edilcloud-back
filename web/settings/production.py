74cec074212ef9b5cb83094aa13cd57969e8c4d6
tommasobellini <dev@tommasobellini.it>
2020-08-22 14:02:00 +0200

    # https protocol enabled

--- web/settings/production.py
+++ web/settings/production.py
@@ -12,6 +12,7 @@ REST_FRAMEWORK_DOCS = {
 }
 SECRET_KEY = '393bf48ry80euefh7ye09euh2f2920hef29r03infbef82938eup13'
 BASE_URL = 'www.edilcloud.it'
+PROTOCOL = 'https'
 
 STRIPE_PUBLISHABLE_KEY = '<your live publishable key here>'
 STRIPE_SECRET_KEY = '<your live secret key here>'

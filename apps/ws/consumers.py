f01fcf8842450170097c6bf30a0ae0ecbb3d8873
tommasobellini <dev@tommasobellini.it>
2020-10-21 00:06:33 +0200

    # first attempt of ws django channels

--- apps/ws/consumers.py
+++ apps/ws/consumers.py
@@ -2,6 +2,9 @@ import json
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
 
+from apps.message.models import MessageProfileAssignment
+
+
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
@@ -26,12 +29,9 @@ class ChatConsumer(WebsocketConsumer):
 
     # Receive message from WebSocket
     def receive(self, text_data):
-        print(text_data)
         text_data_json = json.loads(text_data)
-        print(text_data_json)
         message = text_data_json['message']
         print(message)
-
         # Send message to room group
         async_to_sync(self.channel_layer.group_send)(
             self.room_group_name,
@@ -43,9 +43,24 @@ class ChatConsumer(WebsocketConsumer):
 
     # Receive message from room group
     def chat_message(self, event):
+        print('CHAT MESSAGEEE')
         message = event['message']
-
-        # Send message to WebSocket
-        self.send(text_data=json.dumps({
-            'message': message
-        }))
+        print('read_check' in message)
+        if not 'read_check' in message:
+            # Send message to WebSocket
+            self.send(text_data=json.dumps({
+                'message': message
+            }))
+        else:
+            try:
+                message = message['message']
+                message_id = message['id']
+                dest_id = message['dest']['id']
+                mpa = MessageProfileAssignment.objects.filter(message_id=message_id, profile_id=dest_id)
+                print(mpa)
+                if len(mpa) > 0:
+                    mpa[0].read = True
+                    mpa[0].save()
+                    print(mpa[0].read)
+            except Exception as e:
+                print(e.__str__())

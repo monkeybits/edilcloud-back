55266bbaddc5ae9102ad0c82fb36dc2359e5f285
tommasobellini <dev@tommasobellini.it>
2020-10-27 20:45:50 +0100

    # added asgi.py and daphne

--- web/asgi.py
+++ web/asgi.py
@@ -1,12 +1,6 @@
-"""
-ASGI entrypoint. Configures Django and then runs the application
-defined in the ASGI_APPLICATION setting.
-"""
-
 import os
-import django
-from channels.routing import get_default_application
+from channels.asgi import get_channel_layer
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "web.settings")
-django.setup()
-application = get_default_application()
+
+channel_layer = get_channel_layer()

52f1c8cbada90e30a8d285965e056a4077bbbc54
tommasobellini <dev@tommasobellini.it>
2020-11-09 22:29:51 +0100

    # stripe api

--- apps/payments/api/frontend/urls.py
+++ apps/payments/api/frontend/urls.py
@@ -9,6 +9,16 @@ user_urlpatterns = []
 generic_urlpatterns = []
 
 tracker_urlpatterns = [
+    url(
+        r'^plans/$',
+        generic_views.GenericPlansViews.as_view(),
+        name='generic_view_plans'
+    ),
+    url(
+        r'^checkout/$',
+        generic_views.GenericCheckoutSessionView.as_view(),
+        name='generic_view_pay_checkout'
+    ),
 ]
 
 urlpatterns = user_urlpatterns + generic_urlpatterns + tracker_urlpatterns

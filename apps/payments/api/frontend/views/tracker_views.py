bfb567c508bc9663d42eaa6e09d6186572b36cdd
tommasobellini <dev@tommasobellini.it>
2020-12-22 13:42:53 +0100

    # fix template

--- apps/payments/api/frontend/views/tracker_views.py
+++ apps/payments/api/frontend/views/tracker_views.py
@@ -16,24 +16,24 @@ def home(request):
     customer_id = request.GET.get('customer_id')
     prod_list = []
     stripe.api_key = STRIPE_SECRET_KEY
-    products = Product.objects.all()
-    print('number of plans stripe: ')
-    print(len(products))
-    # products = Product.objects.filter(name__in=['Trial', 'Standard', 'Premium'])
-    # for product in products:
-    #     if product.name == 'Trial':
-    #         prod_list.append(product)
-    # for product in products:
-    #     if product.name == 'Standard':
-    #         prod_list.append(product)
-    # for product in products:
-    #     if product.name == 'Premium':
-    #         prod_list.append(product)
+    # products = Product.objects.all()
+    # print('number of plans stripe: ')
+    # print(len(products))
+    products = Product.objects.filter(name__in=['Trial', 'Standard', 'Premium'])
+    for product in products:
+        if product.name == 'Trial':
+            prod_list.append(product)
+    for product in products:
+        if product.name == 'Standard':
+            prod_list.append(product)
+    for product in products:
+        if product.name == 'Premium':
+            prod_list.append(product)
     if customer_id:
         customer = stripe.Customer.retrieve(customer_id)
         if customer.invoice_settings.default_payment_method:
             has_payment_method = True
-    return render(request, 'payments/home.html', {"products": products, "customer": customer_id, 'has_payment_method': has_payment_method})
+    return render(request, 'payments/home.html', {"products": prod_list, "customer": customer_id, 'has_payment_method': has_payment_method})
 
 def complete(request):
     return render(request, "payments/complete.html")

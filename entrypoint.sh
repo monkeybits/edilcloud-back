bf9e2086729de529b70815e8d1a3989558c9a2e2
tbellini01 <Oneplus3t>
2020-12-29 13:58:22 +0100

    # fix BASE_URL

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,6 @@ python manage.py migrate --noinput && \
 python manage.py createsuperuser --noinput --username "admin" --email "admin@edilcloud.it" && \
 python manage.py shell < web/management/scripts/import_typology.py && \
 python manage.py shell < web/management/scripts/import_category.py || exit 1
+# docker exec -i edilcloud-back_web_1 python manage.py shell < web/management/scripts/import_typology.py
+# docker exec -i edilcloud-back_web_1 python manage.py shell < web/management/scripts/import_category.py
 exec "$@"

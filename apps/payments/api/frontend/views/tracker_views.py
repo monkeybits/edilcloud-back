153f77e8e54d8f5ab20a4efe91d7b0830ee8929e
tbellini01 <Oneplus3t>
2021-09-17 13:53:56 +0200

    # commented subscription_Data param in session create

--- apps/payments/api/frontend/views/tracker_views.py
+++ apps/payments/api/frontend/views/tracker_views.py
@@ -145,9 +145,9 @@ def customer_portal(request):
     session = stripe.billing_portal.Session.create(
         customer=customer_id,
         return_url='https://app.edilcloud.io/apps/todo/all',
-        subscription_data={
-            'default_tax_rates': ['txr_1JOMXdCPJO2Tjuq1Ex7lLrnv', 'txr_1JNfFTCPJO2Tjuq1k6N6s3k2'],
-        }
+        # subscription_data={
+        #     'default_tax_rates': ['txr_1JOMXdCPJO2Tjuq1Ex7lLrnv', 'txr_1JNfFTCPJO2Tjuq1k6N6s3k2'],
+        # }
     )
     return redirect(session.url)
 

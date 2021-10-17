1f9ce8b0031f527b3cc2871d98d2140d4ff130d9
tommasobellini <deev@tommasobellini.it>
2021-10-17 13:12:25 +0200

    # fix notification

--- apps/notify/signals.py
+++ apps/notify/signals.py
@@ -83,7 +83,7 @@ def send_push_notification(notify_obj, recipient, subject, body):
     #     "small_icon": "ic_stat_onesignal_default"
     # }
     # The topic name can be optionally prefixed with "/topics/".
-    topic = 'user{}'.format(recipient.id)
+    topic = 'user{}-{}'.format(os.environ.get('ENV_NAME'), recipient.id)
     print('to topic: {}'.format(topic))
     print('name: {}, surname: {} from company: {}'.format(recipient.first_name, recipient.last_name,
                                                           recipient.company.name))
